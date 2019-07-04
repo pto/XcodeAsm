83e330dbae6bd739104674328eff18d98097a9e0
Peter Olsen <pto@me.com>
2019-07-04 15:59:43 -0400

    Simplest possible assembly program

--- XcodeAsm/ViewController.swift
+++ XcodeAsm/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var output: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        output.text = "The result is \(do_it(43, 7))"
     }
-
 
 }
 
---
+++ XcodeAsm/XcodeAsm-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  XcodeAsm-Bridging-Header.h
+//  XcodeAsm
+//
+//  Created by Peter Olsen on 7/4/19.
+//  Copyright © 2019 Peter Olsen. All rights reserved.
+//
+
+#ifndef XcodeAsm_Bridging_Header_h
+#define XcodeAsm_Bridging_Header_h
+
+long do_it(long a, long b);
+
+#endif /* XcodeAsm_Bridging_Header_h */
